058e4f6200010b38144206f91d231001dcedd2dc
tanigon <tanigon@tanigon.info>
2006-10-01 15:19:36 +0000

    とりあえず２通りのツールボックス管理を使い分けられるように、 こういう案はどうでしょう、というもの
    git-svn-id: https://www.seasar.org/svn/sandbox/s2velocity/trunk@22 3ca3cad3-e10e-0410-a054-cd95625da401

--- s2velocity/src/main/java/org/seasar/velocity/tools/S2VelocityViewServlet.java
+++ s2velocity/src/main/java/org/seasar/velocity/tools/S2VelocityViewServlet.java
@@ -18,7 +18,12 @@ package org.seasar.velocity.tools;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.tools.view.ToolboxManager;
 import org.apache.velocity.tools.view.servlet.VelocityViewServlet;
+import org.seasar.framework.container.ComponentNotFoundRuntimeException;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 
 /**
  * @author <a href="mailto:sato@ouobpo.org">Sato Tadayosi</a>
@@ -26,8 +31,10 @@ import org.apache.velocity.tools.view.servlet.VelocityViewServlet;
  * @version $Id: S2VelocityViewServlet.java,v 1.5 2005/11/16 02:23:18 hanao Exp $
  */
 public class S2VelocityViewServlet extends VelocityViewServlet {
+	/** TOOLBOX_KEY servlet初期化パラメータにこの値が指定されている場合はコンテナ委任型になる */
+    private static final String TOOLBOX_KEY_CONTAINER_MANAGED = "container";
 
-    /**
+	/**
      * Overrides {@link S2VelocityViewServlet} to change toolboxManager
      * implementation from ServletToolboxManager to S2ServletToolboxManager.
      * 
@@ -44,12 +51,29 @@ public class S2VelocityViewServlet extends VelocityViewServlet {
         /* if we have a toolbox, get a manager for it */
         if (file != null)
         {
-            toolboxManager = 
-                S2ServletToolboxManager.getInstance(getServletContext(), file); // NOTE changed here.
+        	if (TOOLBOX_KEY_CONTAINER_MANAGED.equals(file)) {
+                initContainerBasedToolboxManager();
+        	} else {
+	            toolboxManager = 
+	                S2ServletToolboxManager.getInstance(getServletContext(), file); // NOTE changed here.
+        	}
         }
         else
         {
             getVelocityEngine().info("VelocityViewServlet: No toolbox entry in configuration.");
         }
     }
+
+	protected void initContainerBasedToolboxManager() {
+		S2Container container = SingletonS2ContainerFactory.getContainer();
+		if (container != null) {
+		    try {
+		    	toolboxManager = (ToolboxManager) container.getComponent(ToolboxManager.class);
+		    	Velocity.info("S2VelocityViewServlet: Toolbox found in S2Container");
+		    } catch (ComponentNotFoundRuntimeException e) {
+		    	toolboxManager = new ContainerBasedToolboxManager();
+		    	Velocity.info("S2VelocityViewServlet: Toolbox not found in S2Container, using default ContainerBasedToolboxManager.");		    	
+		    }
+		}
+	}
 }
